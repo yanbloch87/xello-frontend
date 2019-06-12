034541630f0733b42c76966041a3c004a8f2deca
Yan Bloch <yan.bloch@startapp.com>
2019-06-12 15:19:59 +0300

    add - tooltip style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'xello-frontend';
+export class AppComponent implements OnInit {
+  buttonATooltip: string;
+  buttonBTooltip: string;
+
+  ngOnInit(): void {
+    this.buttonATooltip = 'Hello, I\'m button a\'s tooltip text';
+    this.buttonBTooltip = 'Hi there, I\'m button b\'s tooltip text';
+  }
 }
